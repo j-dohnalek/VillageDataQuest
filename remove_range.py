44ba7ca4f7981498637d19bd39efd3dbe4680c8b
Jiri Dohnalek <thinkabout3e@googlemail.com>
2018-04-22 20:49:57 +0100

    Update remove_range.py

--- remove_range.py
+++ remove_range.py
@@ -8,13 +8,21 @@ ARR_SIZE = 10
 PATH = 'Data/*.csv'
 JSON_PATH = 'tmp/*.json'
 
+# If the DELETE_FILE value is between 1-3 the program overides area of ARR_SIZE x ARR_SIZE from all csv files in PATH 
+# defined folder with zeros, according to coordinates from JSON_PATH directory coresponding to file name 
+# position{DELETE_FILE}.json. 
+# If the DELETE_FILE value is None the program overides area of ARR_SIZE x ARR_SIZE from all csv files in PATH 
+# defined folder with zeros, according to coordinates from all files in JSON_PATH directory.
 DELETE_FILE = 3
 
 def override():
+    """
+    Overide area of ARR_SIZE x ARR_SIZE square by zeros according to value of
+    DELETE_FILE variable
+    """
     for csvfile in glob.glob(PATH):
-
+        
         arr = np.loadtxt(open(csvfile, "rb"), delimiter=",")
-
         for jsonfile in glob.glob(JSON_PATH):
 
             if DELETE_FILE is None:
@@ -22,9 +30,7 @@ def override():
                     data = json.load(jfile)
                 x = data[0]['x']
                 y = data[0]['y']
-
                 os.remove(jsonfile)
-
                 arr[y-1: (y-1)+ARR_SIZE, x-1:x-1+ARR_SIZE] = np.zeros((ARR_SIZE, ARR_SIZE))
 
             else:
@@ -34,9 +40,7 @@ def override():
                         data = json.load(jfile)
                     x = data[0]['x']
                     y = data[0]['y']
-
                     os.remove(jsonfile)
-
                     arr[y - 1: (y - 1) + ARR_SIZE, x - 1:x - 1 + ARR_SIZE] = np.zeros((ARR_SIZE, ARR_SIZE))
 
         np.savetxt(csvfile, arr.astype(int), fmt='%i', delimiter=",")
@@ -50,3 +54,4 @@ if __name__ == '__main__':
 
     print('Execution Time: %.3fms' % ((end - start) * 1000))
     print('Execution Time: %.3fs' % (end - start))
+    print('Execution Completed')
